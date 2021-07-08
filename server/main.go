83ed0750ac3a6592f46996675c5bdd2a05b3876d
captaincrazybro <myeyesareonmizar@gmail.com>
2021-07-08 15:23:03 -0400

    other stuff

--- server/main.go
+++ server/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	c "github.com/captaincrazybro/literalutil/console"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	port int = 80
+)
+
+func main() {
+	c.Plnf("Webserver started on port %d!", port)
+
+	r := gin.Default()
+
+	r.Run(fmt.Sprintf(":%d", port))
 }
