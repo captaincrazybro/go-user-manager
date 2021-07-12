18e42f6cc2f268f7a8cd8f27e8ac05cb37b0714a
captaincrazybro <myeyesareonmizar@gmail.com>
2021-07-12 11:15:57 -0400

    fixed merge conflict

--- server/routers/api.go
+++ server/routers/api.go
@@ -1,30 +1,18 @@
 package routers
 
 import (
-<<<<<<< HEAD
-	"net/http"
-	"fmt"
-	"os"
-	"io/ioutil"
 	"encoding/json"
-=======
+	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	//"strings"
->>>>>>> 9929aac8263374ffdc8ec0691ce1e109b7f26827
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Login struct {
-<<<<<<< HEAD
-	Users[]		string `form:"users" json:"users" binding:"required"`
-	Passwords[]	string `form:"passwords" json:"passwords" binding:"required"`
-=======
-	User     string `form:"user" json:"user" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
->>>>>>> 9929aac8263374ffdc8ec0691ce1e109b7f26827
+	Users     []string `form:"users" json:"users" binding:"required"`
+	Passwords []string `form:"passwords" json:"passwords" binding:"required"`
 }
 
 func HandleAPI(r *gin.RouterGroup) {
@@ -32,19 +20,12 @@ func HandleAPI(r *gin.RouterGroup) {
 		ctx.String(http.StatusOK, "pong!")
 	})
 
-<<<<<<< HEAD
-	r.GET("", func (ctx *gin.Context) {
+	r.GET("", func(ctx *gin.Context) {
 		contents, _ := ioutil.ReadFile(FILENAME)
 		ctx.String(http.StatusOK, string(contents))
-=======
-	r.GET("", func(ctx *gin.Context) {
-		contents := getData()
-		ctx.String(http.StatusOK, contents)
->>>>>>> 9929aac8263374ffdc8ec0691ce1e109b7f26827
 	})
 
-<<<<<<< HEAD
-	r.POST("", func (ctx *gin.Context) {
+	r.POST("", func(ctx *gin.Context) {
 		var login Login
 		ctx.ShouldBindJSON(&login)
 		fmt.Println(login)
@@ -52,10 +33,3 @@ func HandleAPI(r *gin.RouterGroup) {
 		os.WriteFile(FILENAME, []byte(b), 0666)
 	})
 }
-=======
-func getData() (parsed string) {
-	data, _ := ioutil.ReadFile(FILENAME)
-	parsed = string(data)
-	return parsed
-}
->>>>>>> 9929aac8263374ffdc8ec0691ce1e109b7f26827
