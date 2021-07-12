0ae1c097f4822f9e9413d151501b96b4dc0dd403
captaincrazybro <myeyesareonmizar@gmail.com>
2021-07-12 14:05:44 -0400

    started api login

--- public/js/login/login.jsx
+++ public/js/login/login.jsx
@@ -27,15 +27,36 @@ class Page extends React.Component {
 }
 
 class LoginForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: ""
+        }
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    }
+
+    handleUsernameChange(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handlePasswordChange(event) {
+        this.setState({
+            password: event.target.value
+        })
+    }
     render() {
         return (
             <div id={"login-form"}>
                 <form action={"/api/login"} method={"post"}>
                     <div id={"login-form-fields"}>
                         <label class={"login-form-label"} for={"login-username"}>Username</label><br />
-                        <input id={"login-username"} class={"login-form-field"} type={"text"} /><br />
+                        <input id={"login-username"} class={"login-form-field"} type={"text"} value={this.state.username} onChange={} placeholder={"Enter username"} required/><br />
                         <label class={"login-form-label"} for={"login-password"}>Password</label><br />
-                        <input id={"login-password"} class={"login-form-field"} type={"text"}/><br /><br />
+                        <input id={"login-password"} class={"login-form-field"} type={"text"} value={this.state.password} placeholder={"Enter password"} required/><br /><br />
                     </div>
                     <div id={"login-form-button"}><input id={"login-button"} type={"submit"} value={"Sign In"} /></div>
                 </form>
--- server/routers/api.go
+++ server/routers/api.go
@@ -15,6 +15,19 @@ type Login struct {
 	Passwords []string `form:"passwords" json:"passwords" binding:"required"`
 }
 
+type LoginBody struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type User struct {
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	Permission int    `json:"permission"`
+}
+
+type Users []User
+
 func HandleAPI(r *gin.RouterGroup) {
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "pong!")
@@ -32,4 +45,76 @@ func HandleAPI(r *gin.RouterGroup) {
 		b, _ := json.Marshal(login)
 		os.WriteFile(FILENAME, []byte(b), 0666)
 	})
+
+	r.POST("/login", func(ctx *gin.Context) {
+		var body LoginBody
+		err := ctx.ShouldBind(&body)
+		if err != nil {
+			ctx.JSON(http.StatusOK, MakeErrRes(err))
+			return
+		}
+
+		users, err := GetUsers()
+		if err != nil {
+			ctx.JSON(http.StatusOK, MakeErrRes(err))
+			return
+		}
+
+		user := users.GetUser(body.Username)
+		if user == nil {
+			ctx.JSON(http.StatusOK, MakeErrRes("Username does not exist!"))
+			return
+		}
+
+		if user.Password != body.Password {
+			ctx.JSON(http.StatusOK, MakeErrRes("Incorrect password!"))
+			return
+		}
+
+		// handle session
+		ctx.JSON(http.StatusOK, users)
+	})
+}
+
+func MakeErrRes(v interface{}) gin.H {
+	return gin.H{
+		"type":    "error",
+		"message": fmt.Sprintf("%s", v),
+	}
+}
+
+func GetUsers() (Users, error) {
+	file, err := os.Open("users.json")
+	if os.IsNotExist(err) {
+		file, err = os.Create("users.json")
+		if err != nil {
+			return nil, err
+		}
+
+		_, err := file.Write([]byte("[]"))
+		if err != nil {
+			return nil, err
+		}
+		return []User{}, nil
+	}
+
+	bts, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var users Users
+	err = json.Unmarshal(bts, &users)
+
+	return users, err
+}
+
+func (us Users) GetUser(username string) *User {
+	for _, v := range us {
+		if v.Username == username {
+			return &v
+		}
+	}
+
+	return nil
 }
